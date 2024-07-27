b06cf1e1b4452fc84bac51b8b7a7af9c39df48d4
derlansilva <joderlan.silva@gmail.com>
2024-07-26 21:36:24 -0300

    creating class book

---
+++ library/src/main/java/com/example/project/classes/Book.java
@@ -0,0 +1,25 @@
+package com.example.project.classes;
+
+public class Book {
+    private String title;
+    private boolean isRented = false;
+
+    public Book(String title ){
+        this.title = title;
+    }
+    
+    public String getTitle() {
+        return title;
+    }
+    public void setRented(boolean isRented) {
+        this.isRented = isRented;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return super.toString();
+    }
+}
